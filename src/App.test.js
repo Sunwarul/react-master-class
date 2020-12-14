e9726f6a84a5a683d94e2259f9c661e8c33188cc
Sunwarul Islam <sanoarulslm@gmail.com>
2020-12-14 13:55:49 +0600

    Test scritp updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import {render, screen} from '@testing-library/react';
-import {MonsterRolodex} from './MonsterRolodex/index';
+import { render, screen } from '@testing-library/react';
+// import { MonsterRolodex } from './MonsterRolodex/index';
+import { CrawnClothing } from './CrawnClothing'
 
-test('for searching a monster called Ervin', async () => {
-    render(<MonsterRolodex/>);
+test('find the text HomePage on the document', async () => {
+    render(<CrawnClothing />);
     // const linkElement = screen.getByText(/Loading/i);
-    const monsters = await screen.findByText(/Bauch/i);
+    const monsters = await screen.findByText(/Homepage/i);
     expect(monsters).toBeInTheDocument();
 });
